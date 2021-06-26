d434fe1d1177bd1eacb9c62903c8e7266edff8bf
Adam Vincent <adam.vincent@totalprocessing.com>
2021-06-26 17:04:16 +0100

    create mini server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
 
 func main() {
-	fmt.Println("Hello Favourr")
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		_, err := rw.Write([]byte("Hello World!"))
+		if err != nil {
+			log.Fatalln(err)
+		}
+	})
+
+	if err := http.ListenAndServe(port, http.NewServeMux()); err != nil {
+		log.Fatalln("Error")
+	}
 }
